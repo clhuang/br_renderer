3fac5e791997b1da95d1ec6e6e694aadca94c665
Calvin Huang <clhuang@eneron.us>
2013-10-01 15:49:30 -0700

    formatting

--- RenderGUI.py
+++ RenderGUI.py
@@ -1,43 +1,40 @@
 import numpy as np
-import os
 
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.config import Config
 from kivy.uix.widget import Widget
-from kivy.vector import Vector
-from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.graphics import Rectangle
 
 kivy.require('1.1.2')
 
-class iRenderGui(Widget):
+
+class RenderGUI(Widget):
     azimuth = 20
     altitude = 20
 
-    logOffset=18
+    log_offset = 18
 
     def __init__(self, rend, **kwargs):
-        super(iRenderGui, self).__init__(**kwargs)
-        Config.set('graphics', 'width', rend.projectionXsize)
-        Config.set('graphics', 'height', rend.projectionYsize)
+        super(RenderGUI, self).__init__(**kwargs)
+        Config.set('graphics', 'width', rend.projection_x_size)
+        Config.set('graphics', 'height', rend.projection_y_size)
 
         from kivy.core.window import Window
 
         self.rend = rend
-        self.xsize = rend.projectionXsize
-        self.ysize = rend.projectionYsize
+        self.xsize = rend.projection_x_size
+        self.ysize = rend.projection_y_size
         self.stepsize = rend.stepsize
-        self.distancePerPixel = rend.distancePerPixel
+        self.distance_per_pixel = rend.distance_per_pixel
         self.rendersize = self.ysize * self.xsize
         self.texture = Texture.create(size=(self.xsize, self.ysize))
         self.texture.flip_vertical()
 
-
-        self.xPixelOffset = rend.xPixelOffset;
-        self.yPixelOffset = rend.yPixelOffset;
+        self.x_pixel_offset = rend.x_pixel_offset
+        self.y_pixel_offset = rend.y_pixel_offset
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
@@ -48,39 +45,39 @@ class iRenderGui(Widget):
         self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 'w':  #view up
+        if keycode[1] == 'w':  # view up
             if self.altitude < 90:
                 self.altitude += 2
             else:
                 return
-        elif keycode[1] == 's':  #view down
+        elif keycode[1] == 's':  # view down
             if self.altitude > -90:
                 self.altitude -= 2
             else:
                 return
-        elif keycode[1] == 'a':  #view left
+        elif keycode[1] == 'a':  # view left
             self.azimuth -= 2
-        elif keycode[1] == 'd':  #view right
+        elif keycode[1] == 'd':  # view right
             self.azimuth += 2
-        elif keycode[1] == 'j':  #zoom in
-            self.rend.distancePerPixel *= 0.95
-        elif keycode[1] == 'k':  #zoom out
-            self.rend.distancePerPixel /= 0.95
-        elif keycode[1] == 'u':  #decrease contrast
-            self.logOffset += 1
-        elif keycode[1] == 'i':  #increase contrast
-            self.logOffset -= 1
-        elif keycode[1] == 'up':  #shift view up
-            self.rend.yPixelOffset += 5
-        elif keycode[1] == 'down':  #shift view down
-            self.rend.yPixelOffset -= 5
-        elif keycode[1] == 'left':  #shift view left
-            self.rend.xPixelOffset -= 5
-        elif keycode[1] == 'right':  #shift view right
-            self.rend.xPixelOffset += 5
-        elif keycode[1] == 'o':  #decreases stepsize, increasing resolution
+        elif keycode[1] == 'j':  # zoom in
+            self.rend.distance_per_pixel *= 0.95
+        elif keycode[1] == 'k':  # zoom out
+            self.rend.distance_per_pixel /= 0.95
+        elif keycode[1] == 'u':  # decrease contrast
+            self.log_offset += 1
+        elif keycode[1] == 'i':  # increase contrast
+            self.log_offset -= 1
+        elif keycode[1] == 'up':  # shift view up
+            self.rend.y_pixel_offset += 5
+        elif keycode[1] == 'down':  # shift view down
+            self.rend.y_pixel_offset -= 5
+        elif keycode[1] == 'left':  # shift view left
+            self.rend.x_pixel_offset -= 5
+        elif keycode[1] == 'right':  # shift view right
+            self.rend.x_pixel_offset += 5
+        elif keycode[1] == 'o':  # decreases stepsize, increasing resolution
             self.rend.stepsize *= 0.8
-        elif keycode[1] == 'p':  #increases stepsize, decreasing resolution
+        elif keycode[1] == 'p':  # increases stepsize, decreasing resolution
             self.rend.stepsize /= 0.8
         else:
             return
@@ -88,7 +85,7 @@ class iRenderGui(Widget):
         self.update()
 
     def _on_resize(self, window, width, height):
-        self.rend.projectionXsize, self.rend.projectionYsize = width, height
+        self.rend.projection_x_size, self.rend.projection_y_size = width, height
         self.xsize = width
         self.ysize = height
         self.texture = Texture.create(size=(width, height))
@@ -96,8 +93,8 @@ class iRenderGui(Widget):
         self.update()
 
     def update(self):
-        data = np.log(self.rend.iRender(self.channel, self.azimuth, self.altitude, False))
-        data = (data + self.logOffset) * 255 / (data.max() + self.logOffset)
+        data = np.log(self.rend.i_render(self.channel, self.azimuth, self.altitude, False))
+        data = (data + self.log_offset) * 255 / (data.max() + self.log_offset)
         data = np.clip(data, 0, 255).astype('uint8')
 
         buf = np.getbuffer(data)
@@ -107,20 +104,23 @@ class iRenderGui(Widget):
         with self.canvas:
             self.rect_bg = Rectangle(
                     texture=self.texture, pos=(0, 0),
-                    size = (self.texture.size))
+                    size=(self.texture.size))
         self.canvas.ask_update()
 
 
-class iRenderApp(App):
+class RenderApp(App):
     def __init__(self, rend):
-        super(iRenderApp, self).__init__()
+        super(RenderApp, self).__init__()
         self.rend = rend
+
     def build(self):
-        game = iRenderGui(self.rend)
+        game = RenderGUI(self.rend)
         game.update()
         return game
 
-def setRend(rend, channel):
-    iRenderGui.channel = channel
-    app = iRenderApp(rend)
+
+def set_renderer(rend, channel):
+    RenderGUI.channel = channel
+    app = RenderApp(rend)
     app.run()
+
--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ from pycuda.compiler import SourceModule
 BLOCKSIZE = 256
 MAXGRIDSIZE = 10000000
 
+
 class Renderer:
     '''
     Superclass for rendering 3D models.
@@ -27,24 +28,25 @@ class Renderer:
     To see the variables provided for use in the CUDA module,
     see renderer.h
     '''
-    projectionXsize = 640 #size of the output array
-    projectionYsize = 640
-    stepsize = 0.2 #size of a step along the LOS
-    distancePerPixel = 0.06 #distance (specified by axes) between pixels in the output
+    projection_x_size = 640  # size of the output array
+    projection_y_size = 640
+    stepsize = 0.2  # size of a step along the LOS
+    distance_per_pixel = 0.06  # distance (specified by axes) between pixels in the output
 
-    xPixelOffset = 0 #if 0, the output is centered in the box center--shifts output L/R
-    yPixelOffset = 0 #shifts output U/D
+    x_pixel_offset = 0  # if 0, the output is centered in the box center--shifts output L/R
+    y_pixel_offset = 0  # shifts output U/D
 
-    mod = None #CUDA kernel code
-    textures = {} #stores references to textures to prevent them being cleared
+    mod = None  # CUDA kernel code
+    xaxis = yaxis = zaxis = ixaxis = iyaxis = izaxis = None
+    textures = {}  # stores references to textures to prevent them being cleared
 
-    def loadTexture(self, name, arr):
+    def load_texture(self, name, arr):
         '''
         Loads an array into a texture with a name.
 
         Address by the name in the kernel code.
         '''
-        tex = self.mod.get_texref(name) #x*y*z
+        tex = self.mod.get_texref(name)  # x*y*z
         arr = arr.astype('float32')
 
         if len(arr.shape) == 3:
@@ -63,7 +65,7 @@ class Renderer:
         tex.set_filter_mode(cuda.filter_mode.LINEAR)
         self.textures[name] = tex
 
-    def loadConst(self, name, val):
+    def load_constant(self, name, val):
         '''
         Loads a constant into memory by name in kernel code.
 
@@ -72,85 +74,87 @@ class Renderer:
         '''
         cuda.memcpy_htod(self.mod.get_global(name)[0], val)
 
-    def clearTextures(self):
+    def clear_textures(self):
+        '''
+        Removes all textures from memory.
+        '''
         self.textures = {}
 
     def render(self, azimuth, altitude,
-            consts, tables, splitTables,
-            spec_render, verbose=True):
+               consts, tables, split_tables,
+               spec_render, verbose=True):
         '''
         Renders with a view of azimuth and altitude.
         Loads constants (list of tuples of name, value),
         tables (list of tuples of name, value).
         It splits up the rendering space along the x-axis,
-        loading a corresponding portion of each table in splitTables,
+        loading a corresponding portion of each table in split_tables,
         and rendering a portion at a time by calling spec_render.
 
         spec_render is func with format
         spec_render(self, blocksize, gridsize)
         and is where the CUDA kernel is actually called.
         '''
-        self.clearTextures()
-        viewX, viewY, viewVector = viewAxes(azimuth, altitude)
+        self.clear_textures()
+        view_x, view_y, view_vector = view_axes(azimuth, altitude)
 
         #add axis inverse lookup tables, as well as x axis lookup table
-        tables.extend([
-            ('xtex', np.expand_dims(self.xaxis, 1)),
-            ('ixtex', np.expand_dims(self.ixaxis, 1)),
-            ('iytex', np.expand_dims(self.iyaxis, 1)),
-            ('iztex', np.expand_dims(self.izaxis, 1))])
+        tables.extend([('xtex', np.expand_dims(self.xaxis, 1)),
+                       ('ixtex', np.expand_dims(self.ixaxis, 1)),
+                       ('iytex', np.expand_dims(self.iyaxis, 1)),
+                       ('iztex', np.expand_dims(self.izaxis, 1))])
 
         for tup in tables:
-            self.loadTexture(*tup)
+            self.load_texture(*tup)
         for tup in consts:
-            self.loadConst(*tup)
+            self.load_constant(*tup)
 
         if verbose:
             print('Loaded textures, computed emissivities')
 
         xsplitsize = MAXGRIDSIZE / (self.yaxis.size * self.zaxis.size)
-        numSplits = (self.xaxis.size + xsplitsize - 1) / xsplitsize
+        numsplits = (self.xaxis.size + xsplitsize - 1) / xsplitsize
 
-        self.loadConst('viewVector', viewVector)
-        self.loadConst('viewX', viewX)
-        self.loadConst('viewY', viewY)
-        self.loadConst('ds', np.float32(self.stepsize))
-        self.loadConst('projectionXsize', np.int32(self.projectionXsize))
-        self.loadConst('projectionYsize', np.int32(self.projectionYsize))
-        self.loadConst('distancePerPixel', np.float32(self.distancePerPixel))
-        self.loadConst('xPixelOffset', np.float32(self.xPixelOffset))
-        self.loadConst('yPixelOffset', np.float32(self.yPixelOffset))
+        self.load_constant('viewVector', view_vector)
+        self.load_constant('viewX', view_x)
+        self.load_constant('viewY', view_y)
+        self.load_constant('ds', np.float32(self.stepsize))
+        self.load_constant('projectionXsize', np.int32(self.projection_x_size))
+        self.load_constant('projectionYsize', np.int32(self.projection_y_size))
+        self.load_constant('distancePerPixel', np.float32(self.distance_per_pixel))
+        self.load_constant('xPixelOffset', np.float32(self.x_pixel_offset))
+        self.load_constant('yPixelOffset', np.float32(self.y_pixel_offset))
 
-        for i in range(numSplits):
+        for i in xrange(numsplits):
             xstart = i * xsplitsize
             if xstart + xsplitsize > self.xaxis.size:
                 xsplitsize = self.xaxis.size - xstart
 
             if verbose:
                 print('Rendering x\'-coords ' + str(xstart) + '-' +
-                        str(xstart + xsplitsize) + ' of ' + str(self.xaxis.size))
+                      str(xstart + xsplitsize) + ' of ' + str(self.xaxis.size))
 
-            for tup in splitTables:
+            for tup in split_tables:
                 name, table = tup
-                self.loadTexture(name, table[xstart:xstart+xsplitsize])
+                self.load_texture(name, table[xstart:xstart + xsplitsize])
 
-            dataSize = self.projectionXsize * self.projectionYsize
-            gridSize = (dataSize + BLOCKSIZE - 1) / BLOCKSIZE
+            data_size = self.projection_x_size * self.projection_y_size
+            grid_size = (data_size + BLOCKSIZE - 1) / BLOCKSIZE
 
-            self.loadConst('xstart', np.int32(xstart))
-            self.loadConst('sliceWidth', np.int32(xsplitsize))
+            self.load_constant('xstart', np.int32(xstart))
+            self.load_constant('sliceWidth', np.int32(xsplitsize))
 
-            spec_render(self, BLOCKSIZE, gridSize)
+            spec_render(self, BLOCKSIZE, grid_size)
 
-    def __init__(self, cudaCode):
+    def __init__(self, cuda_code):
         '''
         Initialize with a cuda kernel.
 
         CUDA kernel should include renderer.h to utilize this
         '''
-        self.cudaCode = cudaCode
+        self.cuda_code = cuda_code
 
-    def setAxes(self, xaxis, yaxis, zaxis):
+    def set_axes(self, xaxis, yaxis, zaxis):
         '''
         Sets the x, y, and z axes.
 
@@ -160,23 +164,21 @@ class Renderer:
         self.xaxis = xaxis
         self.yaxis = yaxis
         self.zaxis = zaxis
-        self.xarange = np.ptp(xaxis)
-        self.yarange = np.ptp(yaxis)
-        self.zarange = np.ptp(zaxis)
-        self.mod = SourceModule(self.cudaCode, no_extern_c=True,
-                include_dirs=['/Developer/NVIDIA/CUDA-5.0/samples/common/inc',
-                    os.path.dirname(os.path.abspath(__file__))])
-        self.loadConst('xmin', np.float32(xaxis.min()))
-        self.loadConst('xmax', np.float32(xaxis.max()))
-        self.loadConst('ymin', np.float32(yaxis.min()))
-        self.loadConst('ymax', np.float32(yaxis.max()))
-        self.loadConst('zmin', np.float32(zaxis.min()))
-        self.loadConst('zmax', np.float32(zaxis.max()))
-        self.loadConst('xtotalsize', np.int32(xaxis.size))
+        self.mod = SourceModule(self.cuda_code, no_extern_c=True,
+                                include_dirs=['/Developer/NVIDIA/CUDA-5.0/samples/common/inc',
+                                              os.path.dirname(os.path.abspath(__file__))])
+        self.load_constant('xmin', np.float32(xaxis.min()))
+        self.load_constant('xmax', np.float32(xaxis.max()))
+        self.load_constant('ymin', np.float32(yaxis.min()))
+        self.load_constant('ymax', np.float32(yaxis.max()))
+        self.load_constant('zmin', np.float32(zaxis.min()))
+        self.load_constant('zmax', np.float32(zaxis.max()))
+        self.load_constant('xtotalsize', np.int32(xaxis.size))
 
-        self.ixaxis = normalizedInverseAxis(xaxis)
-        self.iyaxis = normalizedInverseAxis(yaxis)
-        self.izaxis = normalizedInverseAxis(zaxis)
+        self.ixaxis = norm_inverse_axis(xaxis)
+        self.iyaxis = norm_inverse_axis(yaxis)
+        self.izaxis = norm_inverse_axis(zaxis)
+
 
 def numpy3d_to_array(np_array, order=None):
     '''
@@ -193,7 +195,7 @@ def numpy3d_to_array(np_array, order=None):
     elif order.upper() == "F":
         w, h, d = np_array.shape
     else:
-        raise LogicError, "order must be either F or C"
+        raise Exception("order must be either F or C")
 
     descr = ArrayDescriptor3D()
     descr.width = w
@@ -216,12 +218,13 @@ def numpy3d_to_array(np_array, order=None):
 
     return device_array
 
-def viewAxes(azimuth, altitude):
+
+def view_axes(azimuth, altitude):
     '''
     The vectors that correspond to a POV with azimuth and altitude
 
-    viewVector is the line following the LOS,
-    viewX and viewY are the directions you shift
+    view_vector is the line following the LOS,
+    view_x and view_y are the directions you shift
     in space as you move left/right
     '''
     if altitude > 90: altitude = 90
@@ -232,18 +235,20 @@ def viewAxes(azimuth, altitude):
     azimuth = m.radians(int(azimuth))
     altitude = m.radians(int(altitude))
 
-    viewVector = np.array((m.cos(altitude) * m.cos(azimuth), m.cos(altitude) * m.sin(azimuth), m.sin(altitude)))
-    viewX = np.array((m.sin(azimuth), -m.cos(azimuth), 0))
-    viewY = np.cross(viewX, viewVector)
+    view_vector = np.array((m.cos(altitude) * m.cos(azimuth), m.cos(altitude) * m.sin(azimuth), m.sin(altitude)))
+    view_x = np.array((m.sin(azimuth), -m.cos(azimuth), 0))
+    view_y = np.cross(view_x, view_vector)
 
-    return (viewX.astype('float32'), viewY.astype('float32'), viewVector.astype('float32'))
+    return (view_x.astype('float32'), view_y.astype('float32'), view_vector.astype('float32'))
 
-def normalizedInverseAxis(axis):
+
+def norm_inverse_axis(axis):
     '''
     From an axis (corresponds table indices to real coordinates),
     generate an inverse lookup table that will take real coordinates
     (normalized to (0, 1)) and return a normalized (0, 1) lookup index
     '''
     axinverse = interp1d((axis - axis.min()) / np.ptp(axis),
-            np.linspace(0, 1, axis.size))
+                         np.linspace(0, 1, axis.size))
     return axinverse(np.linspace(0, 1, axis.size * 6)).astype('float32')
+
