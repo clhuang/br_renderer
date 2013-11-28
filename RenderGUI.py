5529c5b0bd799aa09675bcc769b25c78af0341b8
Calvin Huang <clhuang@eneron.us>
2013-11-27 16:11:38 -0800

    wheee improved channel selector

--- RenderGUI.py
+++ RenderGUI.py
@@ -8,6 +8,8 @@ from kivy.core.window import Window
 from kivy.properties import NumericProperty, BooleanProperty, VariableListProperty
 from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
 
 kivy.require('1.1.2')
 
@@ -53,14 +55,24 @@ class RenderGUI(Widget):
     y_pixel_offset = NumericProperty(0)
     rend_opacity = BooleanProperty(False)
     channel = NumericProperty(0)
+    log_offset = NumericProperty(6)
 
-    log_offset = 18
     data_str = ('channel:\n'
                 'opacity:\n'
                 'altitude:\n'
                 'azimuth:\n'
                 'dist per pixel:\n'
-                'stepsize:\n')
+                'stepsize:\n'
+                'dynamic range:\n')
+
+    helptext = ('Pan l/r: a/d\n'
+                'Tilt u/d: w/s\n'
+                'Zoom in/out: j/k\n'
+                'Shift l/r: [left]/[right]\n'
+                'Shift u/d: [up]/[down]\n'
+                'Recenter shift: c\n'
+                'Dynamic range inc/dec: i/u\n'
+                'Stepsize inc/dec: p/o\n')
 
     def __init__(self, rend, **kwargs):
         self.texture = Texture.create(size=BUF_DIMENSIONS)
@@ -74,6 +86,21 @@ class RenderGUI(Widget):
 
         self.x_pixel_offset = rend.x_pixel_offset
         self.y_pixel_offset = rend.y_pixel_offset
+
+        # channel dropdown menu
+        channeldropdown = DropDown()
+        for index, x in enumerate(self.rend.channellist()):
+            btn = Button(text=x, height=20, size_hint_y=None)
+            btn.index = index  # lambda is broken
+            btn.bind(on_release=lambda btn: channeldropdown.select((btn.text, btn.index)))
+            channeldropdown.add_widget(btn)
+
+        channelddbutton = Button(text=self.rend.channellist()[0], size=(230, 20),
+                                 size_hint=(None, None), pos=(122, 135))
+        channelddbutton.bind(on_release=channeldropdown.open)
+        channeldropdown.bind(on_select=lambda i, x: setattr(channelddbutton, 'text', x[0]) or
+                             self.update_value('channel', x[1]))
+        self.add_widget(channelddbutton)
 
         self._keyboard_open()
         Window.bind(on_resize=self._on_resize)
@@ -106,10 +133,11 @@ class RenderGUI(Widget):
             self.distance_per_pixel *= 0.95
         elif keycode[1] == 'k':  # zoom out
             self.distance_per_pixel /= 0.95
-        elif keycode[1] == 'u':  # decrease contrast
-            self.log_offset += 1
+        elif keycode[1] == 'u':  # decrease contrast, increasing dyn range
+            self.log_offset += 0.4
         elif keycode[1] == 'i':  # increase contrast
-            self.log_offset -= 1
+            if self.log_offset > 1:
+                self.log_offset -= 0.4
         elif keycode[1] == 'up':  # shift view up
             self.y_pixel_offset += 5
         elif keycode[1] == 'down':  # shift view down
@@ -118,6 +146,8 @@ class RenderGUI(Widget):
             self.x_pixel_offset -= 5
         elif keycode[1] == 'right':  # shift view right
             self.x_pixel_offset += 5
+        elif keycode[1] == 'c':
+            self.x_pixel_offset = self.y_pixel_offset = 0
         elif keycode[1] == 'o':  # decreases stepsize, increasing resolution
             self.stepsize *= 0.8
         elif keycode[1] == 'p':  # increases stepsize, decreasing resolution
@@ -145,7 +175,7 @@ class RenderGUI(Widget):
         self.rend.x_pixel_offset = self.x_pixel_offset
         data = self.rend.i_render(self.channel, self.azimuth, -self.altitude,
                                   opacity=self.rend_opacity, verbose=False)
-        data = np.log(data[0]) if self.rend_opacity else np.log(data)
+        data = np.log10(data[0]) if self.rend_opacity else np.log10(data)
         data = (data + self.log_offset) * 255 / (data.max() + self.log_offset)
         data = np.clip(data, 0, 255).astype('uint8')
         self.buffer_array[:data.shape[0], :data.shape[1]] = data
@@ -163,9 +193,9 @@ class RenderApp(App):
         self.rend = rend
 
     def build(self):
-        self.game = RenderGUI(self.rend)
-        self.game.update()
-        return self.game
+        game = RenderGUI(self.rend)
+        game.update()
+        return game
 
 
 def show_renderer(rend):
